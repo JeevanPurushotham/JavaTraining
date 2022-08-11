105f69a351c7d5c891dfeb590ad9d33cf85394ee
JeevanPurushotham <109270765+JeevanPurushotham@users.noreply.github.com>
2022-08-11 17:03:10 +0530

    Update ReadOnlyMethod.java

--- filehandling/ReadOnlyMethod.java
+++ filehandling/ReadOnlyMethod.java
@@ -4,18 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class ReadOnlyMethod {
-	 public static void main(String[] args) throws IOException
-	    {	
-	    	File myfile = new File("D:\\Programs\\JavaTraining\\src\\Jeevan\\fileOne.txt");
-	    	//making the file read only
-	    	boolean flag = myfile.setReadOnly();	
-	    	if (flag==true)
-	    	{
-	    	   System.out.println("File successfully converted to Read only mode!!");
-	    	}
-	    	else
-	    	{
-	    	   System.out.println("Unsuccessful Operation!!");
-	    	}
-	    }
+	public static void main(String[] args) throws IOException {
+		File myfile = new File("src\\Jeevan\\fileOne.txt");
+		// making the file read only
+		boolean flag = myfile.setReadOnly();
+		if (flag == true) {
+			System.out.println("File successfully converted to Read only mode!!");
+		} else {
+			System.out.println("Unsuccessful Operation!!");
+		}
+	}
 }
