1e6be41ae0a3c50c87e64ed61462c54bd122f5fb
JeevanPurushotham <109270765+JeevanPurushotham@users.noreply.github.com>
2022-08-11 17:02:48 +0530

    Update HiddenProgram.java

--- filehandling/HiddenProgram.java
+++ filehandling/HiddenProgram.java
@@ -4,15 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class HiddenProgram {
-	public static void main(String[] args) throws IOException, SecurityException
-    {	
-    	// Provide the complete file path here
-    	File file = new File("D:\\Programs\\JavaTraining\\src\\Jeevan\\fileOne.txt");
- 
-    	if(file.isHidden()){
-    		System.out.println("The specified file is hidden");
-    	}else{
-    		System.out.println("The specified file is not hidden");
-    	}
-    }
+	public static void main(String[] args) throws IOException, SecurityException {
+		// Provide the complete file path here
+		File file = new File("src\\Jeevan\\fileOne.txt");
+
+		if (file.isHidden()) {
+			System.out.println("The specified file is hidden");
+		} else {
+			System.out.println("The specified file is not hidden");
+		}
+	}
 }
