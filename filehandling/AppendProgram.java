c3786b54a26f8d25118ff9dfbff1604a3a78aa9c
JeevanPurushotham <109270765+JeevanPurushotham@users.noreply.github.com>
2022-08-11 16:41:06 +0530

    Update AppendProgram.java

--- filehandling/AppendProgram.java
+++ filehandling/AppendProgram.java
@@ -7,10 +7,11 @@ import java.io.IOException;
 
 public class AppendProgram {
 	public static void main(String[] args) {
+		BufferedWriter bw = null;
 		try {
 			String content = "This is my content which would be appended " + "at the end of the specified file";
 			// Specify the file name and path here
-			File file = new File("D:\\Programs\\JavaTraining\\src\\jee.txt");
+			File file = new File("src\\Jeevan\\Jeevan.txt");
 			if (!file.exists()) {
 				file.createNewFile();
 			}
@@ -18,16 +19,23 @@ public class AppendProgram {
 			// Here true is to append the content to file
 			FileWriter fw = new FileWriter(file, true);
 			// BufferedWriter writer give better performance
-			BufferedWriter bw = new BufferedWriter(fw);
+			bw = new BufferedWriter(fw);
 			bw.write(content);
 			// Closing BufferedWriter Stream
-			bw.close();
+		
 
 			System.out.println("Data successfully appended at the end of file");
 
 		} catch (IOException ioe) {
 			System.out.println("Exception occurred:");
 			ioe.printStackTrace();
+		}
+		finally {
+			try {
+					bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
